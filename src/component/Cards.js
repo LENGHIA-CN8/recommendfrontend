b7c77e9cf82870b4415e97000c0db85fa3d43787
LENGHIA-CN8 <48158941+LENGHIA-CN8@users.noreply.github.com>
2021-03-18 22:02:23 +0700

    nghia branch up 2.1

--- src/component/Cards.js
+++ src/component/Cards.js
@@ -6,15 +6,15 @@ import './Cards.css'
 export default function Cards(props) {
   const { article } = props;
   return (
-    <div key={article.articleId} className="card " style={{textAlign:"left"}}>
-    <Link to={`/post/${article.articleId}`}>
+    <div key={article.articleID} className="card " style={{textAlign:"left"}}>
+    <Link to={`/post/${article.articleID}`}>
     {/* <img className="card-img-top" style={{padding:"0"}} src={article.image} alt="Card image cap"/> */}
     </Link>
     {/* <Link to={`/post/${article.articleId}`} className='text-decoration-none'> */}
-    <Link to={`/post/${article.articleId}`}>
+    <Link to={`/post/${article.articleID}`}>
     <div className="card-body ">
       {/* <h5 className="card-title " style={{color:"rgb(11, 137, 141)"}}>{article.name}</h5> */}
-      <p className="card-text " style={{color:"grey"}}><small>{article.articleId}...</small></p>
+      <p className="card-text " style={{color:"grey"}}><small>{article.articleID}...</small></p>
     </div>
     </Link>
     {/* </Link> */}
