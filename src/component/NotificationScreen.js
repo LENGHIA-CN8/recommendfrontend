4b6776d3ea6d71ed7b5c74e70452691d3a68d12b
LENGHIA-CN8 <48158941+LENGHIA-CN8@users.noreply.github.com>
2021-03-23 16:38:34 +0700

    ver 3.5

---
+++ src/component/NotificationScreen.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+export default function NotificationScreen(props) {
+  return (
+    <div>
+      <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Bạn chưa đăng nhập!</strong> Hãy đăng nhập để nhận tin gợi ý
+        <button
+          type="button"
+          class="close"
+          data-dismiss="alert"
+          aria-label="Close"
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import Cards from '../component/Cards';
 import InfiniteList from '../component/InfiniteList';
+import NotificationScreen from '../component/NotificationScreen'
+
 
 export default function HomeScreen (props){
     const [state, setState] = useState([]);
+    const userSignin = useSelector((state) => state.userSignin);
+    const { userInfo, error } = userSignin;
     return (
         <div className='Home'>
             {console.log(props.location)}
-            <InfiniteList link={props.location.pathname} state ={state} setState={setState}></InfiniteList>
+            {/* <InfiniteList link={props.location.pathname} state ={state} setState={setState}></InfiniteList> */}
+            {userInfo ? <InfiniteList link={props.location.pathname} state ={state} setState={setState}></InfiniteList> : <NotificationScreen></NotificationScreen>}
         </div>
     )
 }
